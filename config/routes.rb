e2383da6e6454ef804df4a7fab719db53cb27651
brice blanchard <brice.blanchard21@yahoo.fr>
2023-05-23 15:42:46 +0200

    task done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # show all
+  get 'tasks', to: 'tasks#index'
+
+  # create new
+  get 'tasks/new', to: 'tasks#new', as: 'task_new'
+  post 'tasks', to: 'tasks#create'
+
+  # show task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  #edit task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch 'tasks/:id', to: 'tasks#update', as: 'task_update'
+
+  #delete task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
